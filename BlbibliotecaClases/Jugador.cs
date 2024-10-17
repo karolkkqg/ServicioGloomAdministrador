157c90ed3b04c4230bce8caaa4faef99c8c93375
TeDeCanela <153479319+TeDeCanela@users.noreply.github.com>
2024-10-16 20:37:05 -0600

    Patrón singleton en el jugador

--- BlbibliotecaClases/Jugador.cs
+++ BlbibliotecaClases/Jugador.cs
@@ -11,6 +11,8 @@ namespace BlbibliotecaClases
 
     public class Jugador
     {
+        private static Jugador instancia;
+
         [DataMember]
         public String nombreUsuario { get; set; }
         [DataMember]
@@ -25,5 +27,39 @@ namespace BlbibliotecaClases
         public String tipo { get; set; }
         [DataMember]
         public String icono { get; set; }
+
+
+        private Jugador()
+        {
+            nombreUsuario = "Usuario0000";
+            nombre = "UsuarioSinNombre";
+            apellidos = "UsuarioSinApellidos";
+            correo = "correo@delUsuario";
+            contraseña = "si@ScoNtrasa4";
+            tipo = "Invitado";
+            icono = "default_icon.png";
+        }
+
+        public static Jugador GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new Jugador();
+            }
+            return instancia;
+        }
+
+        public void LimpiarSesion()
+        {
+            nombreUsuario = "AC00000";
+            nombre = "Anonimo";
+            apellidos = "SinApellidos";
+            correo = "anonimo@uv.mx";
+            contraseña = "si@ScoNtrasa4";
+            tipo = "sin_usuario";
+            icono = "default_icon.png";
+        }
+
     }
+
 }
--- CalculatorService/ImplementacionServicio.cs
+++ CalculatorService/ImplementacionServicio.cs
@@ -5,7 +5,7 @@ using System.Data.SqlClient;
 using System.ServiceModel;
 using System.Threading;
 
-namespace CalculatorService
+namespace ServicioGloom
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalculatorService" in both code and config file together.
     [ServiceBehavior(ConcurrencyMode= ConcurrencyMode.Reentrant)]
@@ -76,6 +76,8 @@ namespace CalculatorService
         {
             throw new NotImplementedException();
         }
+
+        private void
     }
 
 }
