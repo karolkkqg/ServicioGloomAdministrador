125dbffc7abc9e3661861182f09435f0285ac582
karolkkqg <zs22013661@estudiantes.uv.mx>
2024-11-12 10:02:19 -0600

    ServicioChat

---
+++ ServicioGloomm/ServicioChat.cs
@@ -0,0 +1,81 @@
+﻿using BlbibliotecaClases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioGlomm
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Reentrant)]
+    public class ServicioChat:IChat
+    {
+
+        private Queue<Chat> mensajes = new Queue<Chat>();
+        private Dictionary<string, IChatCallback> jugadoresPartida = new Dictionary<string, IChatCallback>();
+        private IChatCallback respuesta;
+
+
+        public List<Chat> ObtenerHistorialMensajes()
+        {
+            return mensajes.ToList();
+
+        }
+
+
+        public void agregarJugador(string nombreUsuario)
+        {
+            var callback = OperationContext.Current.GetCallbackChannel<IChatCallback>();
+            if (!jugadoresPartida.ContainsKey(nombreUsuario))
+            {
+                jugadoresPartida.Add(nombreUsuario, callback);
+            }
+        }
+
+
+
+
+        public void enviarMensaje(string nomberUsuario, string message)
+
+        {
+
+            if (nomberUsuario.Length > 15)
+            {
+                nomberUsuario = nomberUsuario.Substring(0, 15);
+            }
+
+            respuesta = OperationContext.Current.GetCallbackChannel<IChatCallback>();
+
+
+            Chat mensajeChat = new Chat(nomberUsuario, message);
+
+
+            agregarMensaje(mensajeChat);
+            mandarMensajeAJugadores(mensajeChat);
+            Console.WriteLine($"{nomberUsuario} : {message}");
+
+
+        }
+        private void agregarMensaje(Chat mensajesChat)
+        {
+            mensajes?.Enqueue(mensajesChat);
+        }
+
+        private void mandarMensajeAJugadores(Chat mensajesChat)
+        {
+            foreach (var jugador in jugadoresPartida.Values)
+            {
+                try
+                {
+                    jugador.enviarMensajeCliente(mensajesChat);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al enviar mensaje a cliente: " + ex.Message);
+                }
+            }
+
+        }
+    }
+}
--- ServicioGloomm/ServicioJuegoTablero.cs
+++ ServicioGloomm/ServicioJuegoTablero.cs
@@ -21,7 +21,7 @@ namespace ServicioGloomm
 
     public partial class ServicioJuego : IServicioJuegoTablero
     {
-<<<<<<< HEAD
+
         private ServicioJuego servicioCarta;
         public static readonly Dictionary<string, IJuegoAdministradorCallback> JugadoresConectadosCallback = new Dictionary<string, IJuegoAdministradorCallback>();
         public static readonly Dictionary<string, string> JugadoresConectados = new Dictionary<string, string>();
@@ -30,14 +30,14 @@ namespace ServicioGloomm
         public static readonly List<Carta> CartasSobrantes = new List<Carta>();
         public static readonly Dictionary<string, int> indiceTurnoActual = new Dictionary<string, int>();
         public static readonly Dictionary<string, bool> partidaYaIniciada = new Dictionary<string, bool>();
-=======
+
         private ServicioCarta servicioCarta;
         private static readonly Dictionary<string, IJuegoAdministradorCallback> jugadoresConectadosCallback = new Dictionary<string, IJuegoAdministradorCallback>();
         private static readonly Dictionary<string, string> jugadoresConectados = new Dictionary<string, string>();
         private static readonly Dictionary<string, List<PosicionesJugador>> direccionJugadorEnJuego = new Dictionary<string, List<PosicionesJugador>>();
         private static readonly Dictionary<string, string> TurnsInGameboard = new Dictionary<string, string>();
         private static readonly List<Carta> cartasSobrantes = new List<Carta>();
->>>>>>> 351c70a0508036a7c93ce7726f275ea9c1dd7cda
+
 
         public void IngresarJugadorAJuego(string nombreUsuario, string numeroSala, int numeroJugadores)
         {
@@ -45,7 +45,7 @@ namespace ServicioGloomm
             var callback = OperationContext.Current.GetCallbackChannel<IJuegoAdministradorCallback>();
             if (!jugadoresConectadosCallback.ContainsKey(numeroSala))
             {
-<<<<<<< HEAD
+
                 JugadoresConectadosCallback.Add(nombreUsuario, callback);
                 JugadoresConectados.Add(nombreUsuario, numeroSala);
 
@@ -61,26 +61,24 @@ namespace ServicioGloomm
                 List<Carta> cartasSobrantes = EmpezarJuego(numeroSala);
                 CartasSobrantes.Clear();
                 CartasSobrantes.AddRange(cartasSobrantes);
-=======
+
                 jugadoresConectadosCallback.Add(nombreUsuario, callback);
                 jugadoresConectados.Add(nombreUsuario, numeroSala);
                 List<Carta> cartasSobrantes = EmpezarJuego(numeroSala, numeroJugadores);
                 ServicioJuego.cartasSobrantes.Clear();
                 ServicioJuego.cartasSobrantes.AddRange(cartasSobrantes);
->>>>>>> 351c70a0508036a7c93ce7726f275ea9c1dd7cda
+
             }
         }
 
         private List<Carta> EmpezarJuego(string numeroSala)
         {
-<<<<<<< HEAD
-
             servicioCarta = new ServicioJuego();
             asignarTurnos(numeroSala);
             var cartasSobrantes = servicioCarta.BarajearMazo(numeroSala);
             asignarPrimerTurno(numeroSala);
             return cartasSobrantes;
-=======
+
             VerificarparticipantesConectados(numeroSala, numeroJugadores);
             
                 servicioCarta = new ServicioCarta();
@@ -88,7 +86,7 @@ namespace ServicioGloomm
                 var cartasSobrantes = servicioCarta.BarajearMazo(numeroSala);
                 AsignarPrimerTurno(numeroSala);
                 return cartasSobrantes;
->>>>>>> 351c70a0508036a7c93ce7726f275ea9c1dd7cda
+
             
         }
 
@@ -105,11 +103,10 @@ namespace ServicioGloomm
 
         private void AsignarTurnos(string numeroSala)
         {
-<<<<<<< HEAD
-=======
+
             List<string> jugadorPartida = ObtenerJugadores(numeroSala);
             int cantidadJugadores = jugadorPartida.Count;
->>>>>>> 351c70a0508036a7c93ce7726f275ea9c1dd7cda
+
 
             List<string> jugadores = obtenerJugadores(numeroSala).OrderBy(j => Guid.NewGuid()).ToList();
             int totalJugadores = jugadores.Count;
@@ -138,10 +135,10 @@ namespace ServicioGloomm
         private void AsignarPrimerTurno(string numeroSala)
         {
 
-<<<<<<< HEAD
+
                 List<PosicionesJugador> posiciones = direccionJugadorEnJuego[numeroSala];
                 int totalJugadores = posiciones.Count;
-=======
+
             try
             {
                 jugadoresConectadosCallback[nombreUsuario].RecibirTurno(true);
@@ -154,7 +151,7 @@ namespace ServicioGloomm
             {
                 throw new FaultException<ManejadorExcepciones>(new ManejadorExcepciones(ex.Message));
             }
->>>>>>> 351c70a0508036a7c93ce7726f275ea9c1dd7cda
+
 
             ValidarJugadorIndice(numeroSala, totalJugadores);
 
